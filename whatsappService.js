aebd8abf5ec285b0b29bc3d09efe4f63d8349516
Evangelina <evangelina.gomes@ellecktra.com>
2026-03-17 00:30:52 -0300

    Fix: Add automatic OS detection for Puppeteer executable path

--- whatsappService.js
+++ whatsappService.js
@@ -7,6 +7,8 @@ const client = new Client({
     authStrategy: new LocalAuth({ clientId: "bot-ministraciones" }),
     puppeteer: {
         headless: true,
+        // Detección automática del sistema operativo para usar el ejecutable correcto
+        ...(process.platform !== 'win32' ? { executablePath: '/usr/bin/chromium-browser' } : {}),
         args: [
             '--no-sandbox', 
             '--disable-setuid-sandbox',
